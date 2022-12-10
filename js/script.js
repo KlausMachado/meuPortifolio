17127d6f2717b3d6ad51e001e2abfdfd32afc262
Carla Machado <carlamrosa438@gmail.com>
2022-12-10 01:09:17 -0300

    updates interest

--- js/script.js
+++ js/script.js
@@ -7,3 +7,27 @@ hamburguer.addEventListener("click", () => {
     menu.classList.add("hidden");
   }
 });
+
+const interest = document.querySelectorAll(".interest");
+interest.forEach((props) => {
+  props.addEventListener("click", () => {
+    const div = document.querySelector(".textInterest");
+    const p = div.children;
+    const img = props.classList;
+    if (img.contains("series")) {
+      console.log(p[0]);
+    }
+    if (img.contains("animes")) {
+      console.log(p[1]);
+    }
+    if (img.contains("viajens")) {
+      console.log(p[2]);
+    }
+    if (img.contains("games")) {
+      console.log(p[3]);
+    }
+    if (img.contains("pets")) {
+      console.log(p[4]);
+    }
+  });
+});
